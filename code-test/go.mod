1f77a2a62ae6b13bc3ab8f5be52f001165e5151b
Needrima <oyebodeamirdeen@gmailcom>
2021-12-16 23:32:00 +0100

    commit

--- code-test/go.mod
+++ code-test/go.mod
@@ -2,4 +2,7 @@ module go-codes/code-test
 
 go 1.16
 
-require go.mongodb.org/mongo-driver v1.7.4
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	go.mongodb.org/mongo-driver v1.7.4
+)
--- code-test/main.go
+++ code-test/main.go
@@ -1,71 +1,83 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"os"
+	"html/template"
 )
 
-func checkIfEmailIsRegistered(email string) error {
-	//{"debounce":{"email":"oyebodeamirdeen@gmail.com","code":"5","role":"false","free_email":"true","result":"Safe to Send","reason":"Deliverable","send_transactional":"1","did_you_mean":""},"success":"1","balance":"88"}
-	type Debounce struct {
-		Debounce struct {
-			Email             string `json:"email"`
-			Code              string `json:"code"`
-			Role              string `json:"role"`
-			FreeEmail         string `json:"free_email"`
-			Result            string `json:"result"`
-			Reason            string `json:"reason"`
-			SendTransactional string `json:"send_transactional"`
-			DidYouMean        string `json:"did_you_mean"`
-		} `json:"debounce"`
-		Success string `json:"success"`
-		Balance string `json:"balance"`
-	}
+var (
+	conns = map[*websocket.Conn]bool{}
+	openConns = make(chan *websocket.Conn)
+	closedConns = make(chan *websocket.Conn)
+)
 
-	access_key := os.Getenv("emailValidator_access_key")
-	fmt.Println("access key:", access_key)
-	resp, err := http.Get(fmt.Sprintf("https://api.debounce.io/v1/?api=%s&email=%s", access_key, email))
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Sending a GET on email validator api, may be due to poor internet connection")
-		return errors.New("something went wrong, check your internet connection and try again later")
+		log.Println(err)
+		return
 	}
-	defer resp.Body.Close()
+	defer conn.Close()
 
-	bs, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("could not read email validator response body")
-		return errors.New("something went wrong, check your internet connection and try again later")
-	}
+	conns[conn] = true
 
-	//fmt.Println(string(bs))
+	go func() {
+		openConns <- conn
+	}()
 
-	m := Debounce{}
-
-	err = json.Unmarshal(bs, &m)
-	if err != nil {
-		log.Println("Email response body unmarshal:", err)
-		return errors.New("something went wrong, check your internet connection and try again later")
-	}
-
-	fmt.Printf("Result: %v, Reason: %v\n", m.Debounce.Result, m.Debounce.Reason)
-
-	// result is usually "Safe to Send" and Reason is usually "Deliverable" for registered/reachable emails
-	if m.Debounce.Result != "Safe to Send" || m.Debounce.Reason != "Deliverable" {
-		return errors.New("unregistered")
+	for {
+		select {
+		case conn := <-openConns:
+			go broadcast(conn)
+		case conn := <-closedConns:
+			for item := range conns {
+				if item == conn {
+					delete(conns, item)
+					break
+				}
+			}
+		}
 	}
 
-	return nil
+}
+
+func broadcast(conn *websocket.Conn) {
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Connection closed:", err)
+			break
+		}
+		fmt.Println("Receiving a message from connection:", string(msg))
+
+		for item := range conns {
+			if err := item.WriteMessage(msgType, msg); err != nil {
+				log.Println("Channel closed for writing")
+				closedConns <- item
+			}
+		}
+	}
+
+	closedConns <- conn
 }
 
 func main() {
-	if err := checkIfEmailIsRegistered("oyebodeamirdeen@gmail.com"); err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
+		template.Must(template.ParseFiles("index.html")).Execute(w, nil)
+	})
 
-	fmt.Println("Mail deliverable")
+	http.HandleFunc("/ws", handler)
+
+	http.ListenAndServe(":8000", nil)
 }
