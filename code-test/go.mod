565d26c654e370d3bed23441afb43e0e7f87098a
Needrima <oyebodeamirdeen@gmailcom>
2022-01-24 11:12:09 +0100

    commit

--- code-test/go.mod
+++ code-test/go.mod
@@ -3,6 +3,6 @@ module go-codes/code-test
 go 1.16
 
 require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	go.mongodb.org/mongo-driver v1.7.4
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.2 // indirect
 )
--- code-test/main.go
+++ code-test/main.go
@@ -1,83 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"html/template"
 )
 
-var (
-	conns = map[*websocket.Conn]bool{}
-	openConns = make(chan *websocket.Conn)
-	closedConns = make(chan *websocket.Conn)
-)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+type Person struct {
+	Fname string `json:"fname"`
+	Lname string `json:"lname"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer conn.Close()
-
-	conns[conn] = true
-
-	go func() {
-		openConns <- conn
-	}()
-
-	for {
-		select {
-		case conn := <-openConns:
-			go broadcast(conn)
-		case conn := <-closedConns:
-			for item := range conns {
-				if item == conn {
-					delete(conns, item)
-					break
-				}
-			}
-		}
-	}
-
-}
-
-func broadcast(conn *websocket.Conn) {
-	for {
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Connection closed:", err)
-			break
-		}
-		fmt.Println("Receiving a message from connection:", string(msg))
-
-		for item := range conns {
-			if err := item.WriteMessage(msgType, msg); err != nil {
-				log.Println("Channel closed for writing")
-				closedConns <- item
-			}
-		}
-	}
-
-	closedConns <- conn
+var db = []Person{
+	{"Peter", "Parker"},
+	{"Barry", "Allen"},
+	{"Clark", "Kent"},
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
-		template.Must(template.ParseFiles("index.html")).Execute(w, nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+
+		json.NewEncoder(w).Encode(db)
 	})
 
-	http.HandleFunc("/ws", handler)
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Accept", "application/json, text/plain, */*")
+		w.Header().Set("Content-Type", "x-www-form-urlencoded")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	http.ListenAndServe(":8000", nil)
+		// var p map[string]interface{}
+
+		// if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		// 	log.Println("DECODER: ", err)
+		// 	return
+		// }
+
+		// fmt.Println(p)
+
+		// // db = append(db, p)
+
+		// // json.NewEncoder(w).Encode(p)
+		
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
