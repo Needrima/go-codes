ec7d9f26d14f691b8912c7c6fa99d5d2a4cfeff2
needrima <oyebodeamirdeen@gmail.com>
2022-05-08 23:21:30 +0100

    commit

--- code-test/go.mod
+++ code-test/go.mod
@@ -2,7 +2,4 @@ module go-codes/code-test
 
 go 1.16
 
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/rs/cors v1.8.2 // indirect
-)
+require github.com/gorilla/websocket v1.5.0
--- code-test/main.go
+++ code-test/main.go
@@ -1,52 +1,96 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
 )
 
-type Person struct {
-	Fname string `json:"fname"`
-	Lname string `json:"lname"`
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	conns = map[*websocket.Conn]bool{}
+)
+
+type Chat struct {
+	Type string `json:"type"`
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
 }
 
-var db = []Person{
-	{"Peter", "Parker"},
-	{"Barry", "Allen"},
-	{"Clark", "Kent"},
+func ChatHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	conns[conn] = true
+
+	go func(conn *websocket.Conn) {
+		for {
+			var c Chat
+			err := conn.ReadJSON(&c)
+			if err != nil {
+				fmt.Println("could not read from connection: deleting connection", err)
+				delete(conns, conn)
+				return
+			}
+
+			if c.Type == "new user" {
+				fmt.Printf("new user name: %v joined\n", c.Name)
+			} else if c.Type == "new message" {
+				fmt.Printf("Received message: %v\n", c.Msg)
+			}
+
+			for eachConn := range conns {
+				if err := eachConn.WriteJSON(c); err != nil {
+					fmt.Println("could not write to connection: deleting connection", err)
+					delete(conns, eachConn)
+					continue
+				}
+			}
+		}
+	}(conn)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-
-		json.NewEncoder(w).Encode(db)
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request){
+		switch r.Method {
+		case http.MethodGet:
+			http.ServeFile(w, r, "home.html")
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
 	})
 
-	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Accept", "application/json, text/plain, */*")
-		w.Header().Set("Content-Type", "x-www-form-urlencoded")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		// var p map[string]interface{}
-
-		// if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		// 	log.Println("DECODER: ", err)
-		// 	return
-		// }
-
-		// fmt.Println(p)
-
-		// // db = append(db, p)
-
-		// // json.NewEncoder(w).Encode(p)
+	http.HandleFunc("/newchat", func(w http.ResponseWriter, r *http.Request){
+			groupId := uuid.NewV4()
+			http.Redirect(w, r, "/group?id="+groupId, http.StatusSeeOther)
 		
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request){
+		switch r.Method {
+		case http.MethodGet:
+			http.ServeFile(w, r, "home.html")
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/chat", ChatHandler)
+
+	fmt.Println("visit localhost:8090...")
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
